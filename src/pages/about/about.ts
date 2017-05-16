896493bcd45e830d1093267480ddc62c1cb13b87
zhanglin <yfzl_zhanglin@wddomain3.wandong.com.cn>
2017-05-16 14:22:57 +0800

    init add project files

---
+++ src/pages/about/about.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NavController, ToastController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html'
+})
+export class AboutPage {
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+
+  }
+
+  showToast(position: string) {
+    let toast = this.toastCtrl.create({
+      message: 'Mmmm, buttered toast',
+      duration: 2000,
+      position: position
+    });
+
+    toast.present(toast);
+  }
+
+  showToastWithCloseButton() {
+    const toast = this.toastCtrl.create({
+      message: 'Your files were successfully saved',
+      showCloseButton: true,
+      closeButtonText: 'Ok'
+    });
+    toast.present();
+  }
+
+  showLongToast() {
+    let toast = this.toastCtrl.create({
+      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea voluptatibus quibusdam eum nihil optio, ullam accusamus magni, nobis suscipit reprehenderit, sequi quam amet impedit. Accusamus dolorem voluptates laborum dolor obcaecati.',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+}
